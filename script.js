03970111af6772f633e1733a1524c56c6660191a
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2025-01-03 15:13:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let count = document.querySelector("#counter");
+let incr = document.querySelector(".incrementBtn");
+let number = 0;
+incrementBtn.onclick = function(){
+	number++;
+	alert(number);
+	count.innerHTML = number;
+}
